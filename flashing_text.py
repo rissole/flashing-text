693dd13555e47ac7ac87337a1f8fd65a6e6a9fcd
Alexander Hogue <defaultnamehere@gmail.com>
2013-10-24 17:30:55 +1100

    urls now change the text

--- flashing_text.py
+++ flashing_text.py
@@ -3,23 +3,24 @@ from flask import *
 import base64 as based64
 app = Flask(__name__)
 
-
 @app.route('/')
 def main(): 
-    return render_template("main.html")
+    return render_template("main.htm")
 
 @app.route('/e/<base64_string>')
 def show(base64_string):
-    string = based64.urlsafe_b64decode(base64_string)
-    return render_template("display.html", string=string)
+    #Turns out the type of this string is "unicode" and the based64 module wants "str".
+    string = based64.urlsafe_b64decode(str(base64_string)) #nailed it
+    return render_template("display.htm", string=string)
 
 @app.route('/<gag>')
 def show_gag(gag):
-    return render_template("display.html", string=gag)
+    #buuuut render_template can deal with unicode just fine.
+    return render_template("display.htm", string=gag)
 
 @app.errorhandler(404)
 def custom404(e):
-    return render_template("404.html"), 404
+    return render_template("display.htm", string="[404 PAGE NOT FOUND]"), 404
 
 #LEGACY CODE BELOW, NOT CHANGED SINCE FREELANCER KARMABOARD DAYS
 if __name__ == '__main__':
